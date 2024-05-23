040dad669e1e4db84ce82de1404671a531a863c0
notnuff <113949291+notnuff@users.noreply.github.com>
2024-05-23 21:18:45 +0300

    added endpoint to get random matrix

--- lab-03-starter-project-python/spaceship/routers/api.py
+++ lab-03-starter-project-python/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +7,16 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/random-matrix')
+def randmatrix() -> dict:
+    matrix_a = np.random.randint(0, 10, size=(10, 10))
+    matrix_b = np.random.randint(0, 10, size=(10, 10))
+
+    product = (np.dot(matrix_a, matrix_b))
+
+    return {
+        'matrix_a': matrix_a.tolist(),
+        'matrix_b': matrix_b.tolist(),
+        'product': product.tolist()
+    }
